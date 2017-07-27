236299107857dfe3726df97bd5d2861bb6f1b282
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-27 13:08:17 +0200

    video 130

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+class PostsNew extends Component{
+    render() {
+        return (
+            <div>new post</div>
+        )
+    }
+}
+export default PostsNew;
