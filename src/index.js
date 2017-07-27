ba71af70863953616eca27e332384ef6d9e3c2f2
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-27 12:16:04 +0200

    video 127

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+import promise from 'redux-promise';
+
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/posts_index'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        {/* <Route path="/posts/:id" component={PostShow} />
+        <Route path="/posts/new" component={PostNew} /> */}
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
