c4c87a01665839b4d575231d25f27c95d74a6120
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-27 13:48:01 +0200

    video 132

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 
-class PostsNew extends Component{
+class PostsNew extends Component {
     render() {
         return (
-            <div>new post</div>
+            <form>
+                <Field
+                    name="title"
+                    component={}
+                />
+            </form>
         )
     }
 }
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
