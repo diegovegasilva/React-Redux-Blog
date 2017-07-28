253495d61d80d8d32150266ec2b57079f2fc6764
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-28 09:36:42 +0200

    video 139

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,28 +5,90 @@ import { Link } from 'react-router-dom';
 
 
 class PostsNew extends Component {
-    renderTitleField(field) {
+
+    renderField(field) {
+        const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
+        var fieldType = () => {
+            switch (field.type) {
+                case 'text':
+                default:
+                    return (
+                        <input
+                            className="form-control"
+                            type="text"
+                            {...field.input}
+                        />
+                    )
+                case 'textarea':
+                    return (
+                        <textarea
+                            className="form-control"
+                            {...field.input}
+                        ></textarea>                    
+                    )  
+            }
+        }
         return (
-            <div>
-                <input
-                    type="text"
-                    {...field.input}
-                />
+            <div className={className}>
+                <label>{field.label}</label>
+                {fieldType()}
+                <div className="text-help">
+                    {field.meta.touched ? field.meta.error : ''}
+                </div>    
             </div>
-        )    
+        )
     }
-    
+
+    onSubmit(values) {
+        console.log(values)
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
+                    type="text"    
+                    label="Title"
                     name="title"
-                    component={this.renderTitleField}
+                    component={this.renderField}
                 />
+                <Field
+                    type="text"    
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    type="textarea"    
+                    label="Post content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button className="btn btn-primary" type="submit">Save</button>
             </form>
         )
     }
 }
+
+function validate(values) {
+    const errors = {};
+    if (!values.title) {
+        errors.title = 'Enter a title';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content';
+    }
+
+    return errors;
+}
+
 export default reduxForm({
+    validate: validate,
     form: 'PostsNewForm'
 })(PostsNew);
